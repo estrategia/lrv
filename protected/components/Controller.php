d1646f2f3c9d5df90208e9f47d5e641a41c2f4ce
oscar-monedero <oscar.monedero@eiso.com.co>
2015-10-14 11:36:39 -0500

    Se creo el archivo desktop-om.js y se elimino el codigo añadido en desktop.js

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -33,21 +33,24 @@ class Controller extends CController {
     public $sectorName = "";
 
     public function init() {
-       
+       //*
        if (Yii::app()->detectMobileBrowser->showMobile) {
               $this->isMobile = true;
               $this->layout = '//layouts/mobile';
           }else{
               $this->isMobile = false;
               $this->layout = '//layouts/desktop';
-          }
+          }//*/
+        //$this->isMobile = true;
+        //$this->layout = '//layouts/mobile';
+
        
         $this->pageTitle = Yii::app()->name;
+
         $this->getSectorName();
         $this->registerJs();
         $this->registerCss();
-        
-        if (!$this->isMobile) {
+	if(!$this->isMobile){
             $this->getCategorias();
             $this->getCiudades();
         }
@@ -62,7 +65,7 @@ class Controller extends CController {
                 $this->sectorName .= " - " . $objSubSector->nombreSubSector;
             else if ($objSectorCiudad->objSector->codigoSector != 0)
                 $this->sectorName .= " - " . $objSectorCiudad->objSector->nombreSector;
-        }else {
+        }else{
             $this->sectorName = "Seleccionar ubicación";
         }
     }
@@ -82,10 +85,9 @@ class Controller extends CController {
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/raty/jquery.raty.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/owl-carousel/owl.carousel.min.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/jquerymobile-windows/jqm-windows.mdialog.js", CClientScript::POS_HEAD);
-            Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/common.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/mobile.js", CClientScript::POS_HEAD);
         } else {
-            //Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/jquery/jquery-1.10.0.min.js", CClientScript::POS_HEAD);
+        //    Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/jquery/jquery-1.10.0.min.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/bootstrap/js/bootstrap.min.js", CClientScript::POS_HEAD);
             //Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/mobile.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/jquery-ui/jquery-ui.min.js", CClientScript::POS_HEAD);
@@ -95,9 +97,9 @@ class Controller extends CController {
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/bootstrap/js/bootstrap-slider.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/libs/ad-gallery/jquery.ad-gallery.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . '/libs/bootbox.min.js', CClientScript::POS_END);
-            Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/common.js", CClientScript::POS_HEAD);
             Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/desktop.js", CClientScript::POS_END);
-            /*     Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/npm.js", CClientScript::POS_END); */
+            Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/desktop-om.js", CClientScript::POS_END);
+       /*     Yii::app()->getClientScript()->registerScriptFile(Yii::app()->request->baseUrl . "/js/npm.js", CClientScript::POS_END);*/
         }
     }
 
@@ -124,27 +126,26 @@ class Controller extends CController {
         }
     }
 
-    public function getCiudades() {
-        if (!isset($_SESSION['listciudades'])) {
-            $criteria = new CDbCriteria();
-            $criteria->order = "orden";
-            $_SESSION['listciudades'] = Ciudad::model()->findAll($criteria);
+  public function getCiudades(){
+        if(!isset($_SESSION['listciudades'])){
+            $criteria=new CDbCriteria();
+            $criteria->order="orden";
+            $_SESSION['listciudades']=Ciudad::model()->findAll($criteria);
             Yii::app()->session[Yii::app()->params->sesion['sectorCiudadEntrega']];
         }
     }
-
-    public function getCategorias() {
-        if (!isset($_SESSION['categoriasDesktop'])) {
+    
+    public function getCategorias(){
+        if(!isset($_SESSION['categoriasDesktop'])){
             $categorias = CategoriaTienda::model()->findAll(array(
-                'order' => 't.orden',
-                'condition' => 't.visible=:visible AND t.idCategoriaPadre IS NULL ',
-                'params' => array(
-                    ':visible' => 1,
-                ),
-                'with' => 'listCategoriasHijas',
-            ));
-            $_SESSION['categoriasDesktop'] = $categorias;
+               'order' => 't.orden',
+               'condition' => 't.visible=:visible AND t.idCategoriaPadre IS NULL ',
+               'params' => array(
+                   ':visible' => 1,
+               ),
+               'with' => 'listCategoriasHijas',
+           ));
+           $_SESSION['categoriasDesktop']=$categorias;
         }
     }
-
 }
