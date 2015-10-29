432b85a446102117e394e23919a59da5e1d51f67
oscar-monedero <oscar.monedero@eiso.com.co>
2015-10-29 11:20:37 -0500

    Se agrego la funcionalidad para agregar o eliminar productos a un modulo del tipo lista productos

--- protected/modules/callcenter/controllers/ContenidoController.php
+++ protected/modules/callcenter/controllers/ContenidoController.php
@@ -70,9 +70,149 @@ class ContenidoController extends ControllerOperator {
         ));
     }
 
-    public function actionEditar()
+    public function actionEditar($modulo)
     {
-        print_r($_GET);
+        print_r($modulo);
+    }
+
+    public function actionCrearlistaproductos($modulo)
+    {
+        //print_r($modulo);
+        $objModulo = ModulosConfigurados::model()->findByPk($modulo);
+        //$objProductosModulo = ProductosModulos::model()->findAll("idModulo=:idModulo", array(":idModulo" => $modulo));
+        
+        $this->render('contenidoCrearListaProductos', array(
+            //'objProductosModulo' => $objProductosModulo,
+            'objModulo' => $objModulo
+        ));
+    }
+
+
+    public function actionBuscarproductos()
+    {
+        if (!Yii::app()->request->isPostRequest) {
+            throw new CHttpException(404, 'Solicitud inválida.');
+        }
+
+        $busqueda = Yii::app()->getRequest()->getPost('busqueda');
+        $idModulo = Yii::app()->getRequest()->getPost('idModulo');
+
+        //echo $busqueda." ... ".$modulo;
+        //Yii::app()->end();
+
+        if ($busqueda === null || $idModulo === null) {
+            throw new CHttpException(404, 'Solicitud inválida.');
+        }
+
+        $busqueda = trim($busqueda);
+
+        if (empty($busqueda)) {
+            throw new CHttpException(404, 'Búsqueda no puede estar vacío.');
+        }
+
+        $listProductos = array();
+        $codigosArray = GSASearch($busqueda);
+        //$codigosArray = array(10670, 21461, 22159, 410760, 301280, 10192, 30128, 36085);
+        $codigosStr = implode(",", $codigosArray);
+
+        if (!empty($codigosArray)) {
+            $listProductos = Producto::model()->findAll(array(
+                'with' => array('listImagenes', 'objCodigoEspecial', 'listCalificaciones', 'objCategoriaBI',),
+                'condition' => "t.activo=:activo AND t.codigoProducto IN ($codigosStr)",
+                'params' => array(
+                    ':activo' => 1,
+                )
+            ));
+        }
+
+        $this->renderPartial('buscarProductos', array(
+            'listProductos' => $listProductos,
+            'nombreBusqueda' => $busqueda,
+            'idModulo' => $idModulo
+        ));
     }
     
+
+    public function actionAgregarproductomodulo()
+    {
+        if (!Yii::app()->request->isPostRequest) {
+            throw new CHttpException(404, 'Solicitud inválida.');
+        }
+
+        $producto = Yii::app()->getRequest()->getPost('producto');
+        $idModulo = Yii::app()->getRequest()->getPost('idModulo');
+
+        //echo $producto;
+        //Yii::app()->end();
+
+        if($producto === null || $idModulo === null) {
+            throw new CHttpException(404, 'Solicitud inválida.');
+        }
+
+        $model = new ProductosModulos;
+
+        $model->codigoProducto = $producto;
+        $model->idModulo = $idModulo;
+
+        if($model->save())
+        {
+            $objModulo = ModulosConfigurados::model()->findByPk($idModulo);
+
+            echo CJSON::encode(array('result' => 'ok',
+                'response' => array(
+                    'htmlProductosAgregados' => $this->renderPartial('_listaModuloProductos', array('objModulo' => $objModulo), true, false),
+                    'mensaje' => "Se agrego el producto"
+            )));
+            Yii::app()->end();
+        }
+        else
+        {
+            echo CJSON::encode(array(
+                'result' => 'error',
+                'response' => 'Error al agregar el producto, por favor intente de nuevo',
+            ));
+            Yii::app()->end();
+        }
+    }
+
+
+    public function actionEliminarproductomodulo()
+    {
+        if (!Yii::app()->request->isPostRequest) {
+            throw new CHttpException(404, 'Solicitud inválida.');
+        }
+
+        $idProductoModulo = Yii::app()->getRequest()->getPost('idProductoModulo');
+
+        //echo $producto;
+        //Yii::app()->end();
+
+        if($idProductoModulo === null) {
+            throw new CHttpException(404, 'Solicitud inválida.');
+        }
+
+        $model = ProductosModulos::model()->findByPk($idProductoModulo);
+        $idModulo = $model->idModulo;
+
+        
+        if($model->delete())
+        {
+            $objModulo = ModulosConfigurados::model()->findByPk($idModulo);
+
+            echo CJSON::encode(array('result' => 'ok',
+                'response' => array(
+                    'htmlProductosAgregados' => $this->renderPartial('_listaModuloProductos', array('objModulo' => $objModulo), true, false),
+                    'mensaje' => "Se elimino el producto"
+            )));
+            Yii::app()->end();
+        }
+        else
+        {
+            echo CJSON::encode(array(
+                'result' => 'error',
+                'response' => 'Error al eliminar el producto, por favor intente de nuevo',
+            ));
+            Yii::app()->end();
+        }
+    }
 }
---
+++ protected/modules/callcenter/views/contenido/_listaModuloProductos.php
@@ -0,0 +1,20 @@
+<table class="table table-bordered table-hover table-striped">
+    <tbody>
+        <tr>
+            <th>Imagen</th>
+            <th>Código</th>
+            <th>Producto</th>
+            <th></th>
+        </tr>
+        <?php foreach ($objModulo->objProductosModulos as $fila => $objProductoModulo): ?>
+            <tr>
+                <td><img src="<?php echo Yii::app()->request->baseUrl . $objProductoModulo->objProducto->rutaImagen() ?>" title="<?php echo $objProductoModulo->objProducto->descripcionProducto ?>"> </td>
+                <td><?php echo $objProductoModulo->objProducto->codigoProducto ?></td>
+                <td><?php echo $objProductoModulo->objProducto->descripcionProducto . "<br>" . $objProductoModulo->objProducto->presentacionProducto ?></td>
+                <td>
+                    <?php echo CHtml::link('Eliminar', '#', array('data-producto' => $objProductoModulo->idProductoModulo, 'data-role' => "eliminar-producto-contenido", 'class' => 'btn btn-primary')); ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
