c5f373c1c805d4b7852530e148147da943b7eca8
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-11-09 07:46:12 -0500

    Ajuste minicarro

--- protected/views/carro/d_canasta.php
+++ protected/views/carro/d_canasta.php
@@ -1,11 +1,12 @@
-<div class="btn-group">
-    <button type="button" class="btn btn-danger">Action</button>
-    <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span class="caret"></span>
-        <span class="sr-only">Carro de compras</span>
-    </button>
-    <ul class="dropdown-menu">
-        <li>Hay <?php echo Yii::app()->shoppingCart->getCount(); ?> producto(s) en el carro</li>
+<div class="btn-group minicar">
+  <a href="#" data-toggle="dropdown" data-placeholder="false" class="dropdown-toggle">
+    <span><img class="ico-carrito" src="<?php echo Yii::app()->request->baseUrl; ?>/images/desktop/ico-carrito.png" alt=""></span>
+    <span id="cantidad-productos" class="cantidad-productos"><?php echo Yii::app()->shoppingCart->getCount(); ?></span>
+    <p style="color: #A3A3A3;">Productos</p>
+  </a>
+    
+    <ul class="dropdown-menu noclose pull-right">
+        <li class="top">Hay <?php echo Yii::app()->shoppingCart->getCount(); ?> producto(s) en el carro</li>
         <?php if (!Yii::app()->shoppingCart->isEmpty()): ?>
             <?php if (Yii::app()->shoppingCart->getCount() > 1): ?>
                 <li>
@@ -55,7 +56,14 @@
                     <?php endif; ?>
                 </div>
             </li>
-
+        <?php else: ?>
+            <li>
+                <div class="row">
+                    <div class="col-sm-12 carro-descripcion sin-productos">
+                        No hay productos en el carrito
+                    </div>
+                </div>
+            </li>
         <?php endif; ?>
 
 
