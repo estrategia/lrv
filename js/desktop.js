d593bda98a0d1679cf1127f689ab27346e87550c
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-11-09 17:24:22 -0500

    script para cuadricula

--- js/desktop.js
+++ js/desktop.js
@@ -4,3 +4,14 @@
  * and open the template in the editor.
  */
  $('#items-page').select2();
+ $('.viewsList .btn').click(function(){
+ 	atribu=$(this).attr('data-type');
+ 	if (atribu=='lineal') {
+ 		$('#lista-productos').removeClass('list_cuadricula');
+ 		$('#lista-productos').addClass('list_lineal')
+ 		$('#lista-productos.list_lineal .listaProductos li .content-txt2').addClass('row');
+ 		$('#lista-productos.list_lineal .listaProductos li .content-txt2 .img-list-products').addClass('col-sm-3');
+ 		$('#lista-productos.list_lineal .listaProductos li .content-txt2 .content_product').addClass('col-sm-5');
+ 		$('#lista-productos.list_lineal .listaProductos li .content-txt2 .botones-list').addClass('col-sm-4');
+ 	}
+ });
