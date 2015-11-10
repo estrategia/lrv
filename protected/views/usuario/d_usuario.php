de656cca8229ee55ac82686d9aa8fd465ad6e49e
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-11-10 01:28:37 -0500

    se adiciono el contendor

--- protected/views/usuario/d_usuario.php
+++ protected/views/usuario/d_usuario.php
@@ -1,8 +1,10 @@
-<div class="row">
-    <div class="col-md-3">
-        <?php $this->renderPartial('_d_menu'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <?php $this->renderPartial('_d_menu'); ?>
+        </div>
+        <div class="col-md-9">
+            <?php $this->renderPartial($vista, $params); ?>
+        </div>  
     </div>
-    <div class="col-md-9">
-        <?php $this->renderPartial($vista, $params); ?>
-    </div>  
 </div>
