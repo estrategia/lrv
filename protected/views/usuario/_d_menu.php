cb38d0f33214492da13b85728d5d7553a8551aff
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-11-10 01:50:48 -0500

    clase menu-user

--- protected/views/usuario/_d_menu.php
+++ protected/views/usuario/_d_menu.php
@@ -1,40 +1,40 @@
-<ul class="nav nav-pills nav-stacked">
+<ul class="nav nav-pills nav-stacked menu-user">
     <li class="active">
         <a href="<?php echo CController::createUrl('/usuario/infoPersonal') ?>" >
             Informaci&oacute;n personal
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/contrasena') ?>" >
             Cambiar contrase&ntilde;a
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/direcciones') ?>" >
             Direcciones de despacho
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/listapedidos') ?>" >
             Listado de pedidos
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/listapersonal') ?>" >
             Listas personales
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/listacotizaciones') ?>" >
             Cotizaciones
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/pagoexpress') ?>" >
             Pago express
         </a>
     </li>
-    <li class="active">
+    <li>
         <a href="<?php echo CController::createUrl('/usuario/salir') ?>" >
             Cerrar sesi&oacute;n
         </a>
