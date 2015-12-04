93a5a1b84e7cd3dd0bcd882b00387550d3aa75c6
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-12-04 05:43:24 -0500

    Ajuste de ubicacion

--- protected/views/sitio/d_ubicacion.php
+++ protected/views/sitio/d_ubicacion.php
@@ -1,69 +1,54 @@
 <div class="container">
     <div class="space-1"></div>
-    <section>
-        <div class="row center">
-            <h3>Selecciona tipo de entrega y ubicaci&oacute;n </h3>
+    <section class="selectedUbicacion">
+        <div class="center">
+            <h3>SELECCIONA EL TIPO DE ENTREGA Y LA UBICACI&Oacute;N DONDE DESEAS QUE ENTREGUEMOS TU PEDIDO</h3>
         </div>
+        <div class="blocktipoentrega">
         <div class="row">
             <div class="col-md-6">
-                <div class="row">
+                    <h4>TIPO DE ENTREGA</h4>
                     <div data-role="tipoentrega" data-tipo="<?php echo Yii::app()->params->entrega['tipo']['presencial']; ?>" class="tipo-entrega<?php echo ($tipoEntrega == Yii::app()->params->entrega['tipo']['presencial'] ? " activo" : "") ?>">
-                        Quiero pasar por el pedido
+                        <div class="ico_ubi icoRecoger"></div>
+                        <div class="inner_tipoentrega">Quiero pasar por el pedido</div>
+                        <div class="clear"></div>
                     </div>
-                </div>
                 <div class="space-1"></div>
-                <div class="row">
                     <div data-role="tipoentrega" data-tipo="<?php echo Yii::app()->params->entrega['tipo']['domicilio']; ?>" class="tipo-entrega<?php echo ($tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio'] ? " activo" : "") ?>">
-                        Quiero que me lo entreguen a domicilio
+                        <div class="ico_ubi icoEntrega"></div>
+                        <div class="inner_tipoentrega">Quiero que me lo entreguen a domicilio</div>
+                        <div class="clear"></div>
                     </div>
-                </div>
             </div>
 
             <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-12" >
+                <h4>UBICACI&Oacute;N DE ENTREGA</h4>
                         <?php echo CHtml::htmlButton('Usar la ubicación de tu dispositivo', array('class' => 'btn btn-danger btn-ciudad', 'data-role' => 'ubicacion-gps')); ?>
-                    </div>
-                </div>
                 <div class="space-1"></div>
                 <?php if (!Yii::app()->user->isGuest): ?>
-                    <div class="row">
-                        <div class="col-md-12" >
                             <?php echo CHtml::htmlButton('Usar tus direcciones', array('class' => 'btn btn-danger btn-ciudad', 'data-role' => 'ubicacion-direccion')); ?>
-                        </div>
-                    </div>
                     <div class="space-1"></div>
                 <?php endif; ?>
-                <div class="row">
-                    <div class="col-md-12" >
                         <?php echo CHtml::htmlButton('Buscar ubicaci&oacute;n', array('class' => 'btn btn-danger btn-ciudad', 'data-role' => 'ubicacion-mapa')); ?>
-                    </div>
-                </div>
                 <div class="space-1"></div>
                 <?php if ($objSectorCiudad != null): ?>
                     <div class="space-1"></div>
-                    <div class="row">
-                        <div class="col-md-12">
                             <?php echo CHtml::link('Continuar en ' . $this->sectorName, $urlRedirect, array('class' => 'btn btn-warning btn-ciudad', 'data-mini' => 'true', 'data-ajax' => 'false')); ?>
-                        </div>
                     </div>
                 <?php endif; ?>
                 <div class="space-1"></div>
             </div>
         </div>
-        <div class="row">
+        </div>
             <form id="form-ubicacion"  method="post" action="<?php echo $this->createUrl("/sitio/ubicacionSeleccion") ?>">
                 <input id="ubicacion-seleccion-entrega" type="hidden" name="entrega" value="<?php echo $tipoEntrega ?>">
                 <input id="ubicacion-seleccion-direccion" type="hidden" name="direccion" value="">
                 <input id="ubicacion-seleccion-ciudad" type="hidden" name="ciudad" value="">
                 <input id="ubicacion-seleccion-sector" type="hidden" name="sector" value="">
-                <div class="row center">
-                    <div class="col-md-12">
+                <div class="center">
                         <button data-role="ubicacion-seleccion" class="btn btn-default" type="button">Aceptar</button>
-                    </div>
                 </div>
             </form>
-        </div>
     </section>
 </div>
 
@@ -77,7 +62,7 @@
                         <select class="form-control ciudades" data-role="ciudad-despacho-map" style="width: 100%">
                             <option value="">Seleccione ...</option>
                             <?php foreach ($listCiudadesSectores as $ciudad): ?>
-                                <option data-latitud="<?php echo $ciudad->latitudGoogle ?>" data-longitud="<?php echo $ciudad->longitudGoogle ?>" value="<?php echo $ciudad->codigoCiudad ?>-0"><?php echo $ciudad->nombreCiudad ?></option>
+                                <option data-latitud="<?php #echo $ciudad->latitudGoogle ?>" data-longitud="<?php #echo $ciudad->longitudGoogle ?>" value="<?php echo $ciudad->codigoCiudad ?>-0"><?php echo $ciudad->nombreCiudad ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
