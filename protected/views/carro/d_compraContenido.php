800c8a94d1730b815397f1ec7b47fca41e8fcec5
estrategia <andres.tabares@eiso.com.co>
2015-12-04 10:45:26 -0500

    Se ajusta el diseño del resumen del pedido al confirmar la compra

--- protected/views/carro/d_compraContenido.php
+++ protected/views/carro/d_compraContenido.php
@@ -1,67 +1,71 @@
-<div class="">
-    <h2 class="text-center title-desp">¡Gracias por su compra!</h2>
 
+<h2 class="text-center title-desp">¡Gracias por su compra!</h2>
+<div class='container'>
     <div class="blockPago">
-        <?php if ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio']): ?>
-            <h3>Datos Compra</h3>
+        <!-- <?php if ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio']): ?>
+             <h3>Datos Compra</h3>
         <?php elseif ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['presencial']): ?>
-            Datos Pedido
-        <?php endif; ?>
-        <table  class="infoCompra">
-            <tr>
-                <td>Número compra</td>
-                <td><?php echo $objCompra->idCompra ?></td>
-            </tr>
-            <tr>
-                <td>Fecha/Hora compra</td>
-                <td><?php echo $objCompra->fechaCompra ?></td>
-            </tr>
+                         Datos Pedido
+        <?php endif; ?> -->
 
-            <?php if ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio']): ?>
-                <tr class="rowRed">
-                    <td>Fecha/Hora entrega</td>
-                    <td><?php echo $objCompra->fechaEntrega ?></td>
-                </tr>
-            <?php elseif ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['presencial']): ?>
-                <tr class="rowRed">
-                    <td>Punto de venta entrega</td>
-                    <td>
-                        <?php echo $modelPago->listPuntosVenta[1][$modelPago->indicePuntoVenta][2] ?>
-                        <br>
-                        <?php echo $modelPago->listPuntosVenta[1][$modelPago->indicePuntoVenta][3] ?>
-                    </td>
-                </tr>
-            <?php endif; ?>
+        <p style="text-align:right;font-size:14px" >
+            <strong>
+                <span style="font-size:12pt">Número compra: </span>
+            </strong><span style="font-size:12pt;color:#c20000">
+                <?php echo $objCompra->idCompra ?></span>
+        </p>
+        <p style="text-align:right;font-size:14px" >
+            <strong>
+                <span style="font-size:12pt">Fecha/Hora compra: </span>
+            </strong><span style="font-size:12pt;color:#c20000">
+                <?php echo $objCompra->fechaCompra ?></span>
+        </p>
+        <p style="text-align:right;font-size:14px" >
+            <strong>
+                <span style="font-size:12pt">
+                    <?php if ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio']): ?>
+                        Fecha/Hora entrega
+                    <?php else: ?>
+                        Punto de venta entrega
+                    <?php endif; ?>       
+                </span>
+            </strong>
+            <span style="font-size:12pt;color:#c20000">
+                <?php if ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio']): ?>
+                    <?php echo $objCompra->fechaEntrega ?>
+                <?php else: ?>
+                    <?php echo $modelPago->listPuntosVenta[1][$modelPago->indicePuntoVenta][2] ?>
+                    <br>
+                    <?php echo $modelPago->listPuntosVenta[1][$modelPago->indicePuntoVenta][3] ?>
+                <?php endif; ?> 
+            </span>
+        </p>
 
-        </table> 
-    </div>
+    </div> 
 
     <?php if ($objCompra->tipoEntrega == Yii::app()->params->entrega['tipo']['domicilio']): ?>
-        <div class="blockPago">
-            <h3>Datos de despacho</h3>
-            <table class="infoCompra">
+        <table cellpadding="10" style="font-size:14px;border-collapse:collapse;border-spacing:0px;width:99.9%;border-bottom:1px solid #dddddd;margin-bottom:24px">
+            <tbody>
                 <tr>
-                    <td>Nombre destinatario</td>
-                    <td><?php echo $objCompraDireccion->nombre ?></td>
+                    <td width="50%" style="border-bottom:1px solid #dddddd;padding:0" colspan="2">
+                        <h4 style="color:#666666"> Datos de despacho</h4>
+                    </td>
                 </tr>
                 <tr>
-                    <td>Ciudad</td>
-                    <td><?php echo $this->sectorName ?></td>
+                    <td valign="top" width="50%" style="border-left:1px solid #dddddd;border-right:1px solid #dddddd;border-bottom:1px solid #dddddd;color:#444444;background:#f9f9f9">
+                        <p><b>Nombre destinatario: </b><?php echo $objCompraDireccion->nombre ?></p>
+                        <p><b>Ciudad: </b><?php echo $this->sectorName ?></p>
+                        <p><b>Direcci&oacute;n: </b><?php echo $objCompraDireccion->direccion ?></p>
+                    </td>
+                    <td width="50%" valign="top" style="border-right:1px solid #dddddd;color:#555555;background:#f9f9f9">
+                        <p><b>Barrio: </b><?php echo $objCompraDireccion->barrio ?></p>
+                        <p><b>Tel&eacute;fono: </b><?php echo $objCompraDireccion->telefono ?></p>
+                        <p><b>Forma de pago: </b><?php echo $objCompra->objFormaPagoCompra->objFormaPago->formaPago ?></p>
+                    </td>
                 </tr>
-                <tr>
-                    <td>Dirección</td>
-                    <td><?php echo $objCompraDireccion->direccion ?></td>
-                </tr>
-                <tr>
-                    <td>Barrio</td>
-                    <td><?php echo $objCompraDireccion->barrio ?></td>
-                </tr>
-                <tr>
-                    <td>Teléfono</td>
-                    <td><?php echo $objCompraDireccion->telefono ?></td>
-                </tr>
-            </table>
-        </div>
+            </tbody>
+        </table>    
+
     <?php endif; ?>
     <div class="space-2"></div>
 
@@ -72,11 +76,7 @@
         </div>
     <?php endif; ?>
 
-
-
     <div id="div-carro">
         <?php $this->renderPartial('_d_carro', array('lectura' => true)); ?>
     </div>
-
-
 </div>
