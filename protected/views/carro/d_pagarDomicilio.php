231a2c80734a748d6ba1b46f115d927460802b17
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-12-04 04:17:05 -0500

    ajuste paginador

--- protected/views/carro/d_pagarDomicilio.php
+++ protected/views/carro/d_pagarDomicilio.php
@@ -1,27 +1,29 @@
 <section>
     <div class="container">
-        <div class="col-md-12">
-            <div class="steps">
-                <ul>
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 1 ? "active" : "") ?> border-solid-right">
-                        <div class="iconStep step1"></div>
-                        <div class="text"><p>1.Despacho</p></div>
-                    </li>
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 2 ? "active" : "") ?> border-solid-right">
-                        <div class="iconStep step2"></div>
-                        <div class="text"><p>&nbsp;2.Entrega</p></div>
-                    </li>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="steps">
+                    <ul>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 1 ? "active" : "") ?> border-solid-right">
+                            <div class="iconStep step1"></div>
+                            <div class="text"><p>1.Despacho</p></div>
+                        </li>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 2 ? "active" : "") ?> border-solid-right">
+                            <div class="iconStep step2"></div>
+                            <div class="text"><p>&nbsp;2.Entrega</p></div>
+                        </li>
 
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 3 ? "active" : "") ?> border-solid-right">
-                        <div class="iconStep step3"></div>
-                        <div class="text"><p>3.Pago</p></div>
-                    </li>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 3 ? "active" : "") ?> border-solid-right">
+                            <div class="iconStep step3"></div>
+                            <div class="text"><p>3.Pago</p></div>
+                        </li>
 
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 4 ? "active" : "") ?>">
-                        <div class="iconStep step4"></div>
-                        <div class="text"><p>4.Confirmación</p></div>
-                    </li>
-                </ul>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 4 ? "active" : "") ?>">
+                            <div class="iconStep step4"></div>
+                            <div class="text"><p>4.Confirmación</p></div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -46,16 +48,12 @@
         </div>
         <div class="space-1"></div>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-sm-12">
                 <?php if ($pasoAnterior !== null): ?>
-                    <div class="col-md-offset-1 col-md-5">
                         <button class="editar" id="btn-carropagar-anterior" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoAnterior ?>">Atr&aacute;s</button>
-                    </div>
                 <?php endif; ?>
                 <?php if ($pasoSiguiente !== null): ?>
-                    <div class="col-md-offset-3 col-md-3">
                         <button class="adicionar" id="btn-carropagar-siguiente" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoSiguiente ?>">Continuar</button>
-                    </div>
                 <?php endif; ?>
             </div>
         </div>
