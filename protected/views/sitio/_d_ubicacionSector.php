e8853d883cd723f36ced5e39a82c209c4fbf05e6
msanchez-eiso <miguel.sanchez@eiso.com.co>
2015-12-04 16:33:22 -0500

    Gmap select de sectores sin microsectores

--- protected/views/sitio/_d_ubicacionSector.php
+++ protected/views/sitio/_d_ubicacionSector.php
@@ -3,11 +3,7 @@
         <select data-role="sector-despacho-map" class='form-control ciudades' style="width: 100%">
             <option value="">Seleccione sector ...</option>
             <?php foreach ($sectores as $sector): ?>
-                <optgroup label="<?php echo $sector->getNombreSector() ?>">
-                    <?php foreach ($sector->listPuntoReferencias as $puntoReferencia): ?>
-                        <option value="<?php echo $sector->idSectorPuntoReferencia ?>" data-latitud="<?php echo $sector->objSectorCiudad->latitudGoogle ?>" data-longitud="<?php echo $sector->objSectorCiudad->longitudGoogle ?>"> <?php echo $puntoReferencia->nombreReferencia ?></option>
-                    <?php endforeach; ?>            
-                </optgroup>
+            <option value="<?php echo $sector->idSectorPuntoReferencia ?>" data-latitud="<?php echo $sector->objSectorCiudad->latitudGoogle ?>" data-longitud="<?php echo $sector->objSectorCiudad->longitudGoogle ?>"> <?php echo $sector->getNombreSector() ?></option>
             <?php endforeach; ?>
         </select>
     </div>
