7b91c169e0efd58469f65986255a12804398a1f9
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-12-04 04:16:22 -0500

    ajuste paginador

--- protected/views/carro/d_pasosPresencial.php
+++ protected/views/carro/d_pasosPresencial.php
@@ -1,6 +1,6 @@
 <section>
     <div class="container">
-        <div class="col-md-12">
+        <div class="col-sm-12">
             <div class="steps">
                 <ul>
                     <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 2 ? "active" : "") ?> border-solid-right">
@@ -34,16 +34,15 @@
 
 <section>
     <div class="container">
-        <div class="col-md-12 desplegables">
-            <?php $this->renderPartial('_d_paso' . Yii::app()->params->pagar['pasos'][$paso], $parametros); ?>
-
-            <div class="col-md-12">
-                <?php if ($pasoAnterior !== null): ?>
-                <div class="col-md-3"><button class="editar" id="btn-carropagar-anterior" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoAnterior ?>">Atr&aacute;s</button></div>
-                <?php endif; ?>
-                <?php if ($pasoSiguiente !== null): ?>
-                    <div class="col-md-3"><button class="adicionar" id="btn-carropagar-siguiente" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoSiguiente ?>">Continuar</button></div>
-                <?php endif; ?>
+        <div class="row">
+            <div class="col-sm-12 desplegables">
+                <?php $this->renderPartial('_d_paso' . Yii::app()->params->pagar['pasos'][$paso], $parametros); ?>
+                    <?php if ($pasoAnterior !== null): ?>
+                    <button class="editar" id="btn-carropagar-anterior" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoAnterior ?>">Atr&aacute;s</button><
+                    <?php endif; ?>
+                    <?php if ($pasoSiguiente !== null): ?>
+                    <button class="adicionar" id="btn-carropagar-siguiente" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoSiguiente ?>">Continuar</button>
+                    <?php endif; ?>
             </div>
         </div>
     </div>
