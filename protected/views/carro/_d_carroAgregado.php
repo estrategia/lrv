cd93a266417fd195de25dcb6937d94e1b0c1425c
msanchez-eiso <miguel.sanchez@eiso.com.co>
2015-12-04 13:23:18 -0500

    Desktop mensaje agregar carro y gmaps ubicacion

--- protected/views/carro/_d_carroAgregado.php
+++ protected/views/carro/_d_carroAgregado.php
@@ -1,18 +1,8 @@
-<div data-role='main'>
-    <div class='' data-role='content' role='main'>
-        <div id="<?php echo $objProducto->codigoProducto ?>" class="col-md-1">
-            <?php $listImagen=$objProducto->listImagenes;?>
-                   <?php if (empty($listImagen)): ?>
-                         <div class="item"><img class="img-responsive" src="<?php echo Yii::app()->request->baseUrl . Yii::app()->params->producto['noImagen']['grande']; ?>" alt="<?php echo $objProducto->descripcionProducto ?>" title="<?php echo $objProducto->descripcionProducto ?>"></div>
-                   <?php else: ?>
-                       <?php foreach ($listImagen as $imagen): ?>
-                         <div class="item"><img class="img-responsive" src="<?php echo Yii::app()->request->baseUrl . Yii::app()->params->carpetaImagen['productos'][YII::app()->params->producto['tipoImagen']['mini']] . $imagen->rutaImagen; ?>" alt="<?php echo $imagen->nombreImagen ?>" title="<?php echo $imagen->tituloImagen ?>"></div>
-                       <?php     break;?>
-                       <?php endforeach; ?>
-                   <?php endif; ?>
-        </div>
-        <div id="<?php echo $objProducto->codigoProducto ?>" class="col-md-10">
-            Se ha adicionado <?php echo $objProducto->descripcionProducto ?> <?php echo $objProducto->presentacionProducto ?> al carrito.
-        </div>
-    </div> 
+<div class='row'>
+    <div id="<?php echo $objProducto->codigoProducto ?>" class="col-md-2">
+        <img class="ajustada" src="<?php echo Yii::app()->request->baseUrl . $objProducto->rutaImagen(); ?>" alt="<?php echo $objProducto->descripcionProducto ?>" title="<?php echo $objProducto->descripcionProducto ?>">
+    </div>
+    <div id="<?php echo $objProducto->codigoProducto ?>" class="col-md-10">
+        Se ha adicionado <?php echo $objProducto->descripcionProducto ?> <?php echo $objProducto->presentacionProducto ?> al carrito.
+    </div>
 </div>
--- protected/views/carro/d_pasosPresencial.php
+++ protected/views/carro/d_pasosPresencial.php
@@ -1,23 +1,23 @@
 <section>
     <div class="container">
-        <div class="col-sm-12">
-            <div class="steps">
-                <ul>
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 2 ? "active" : "") ?> border-solid-right">
-                        <div class="iconStep step2"></div>
-                        <div class="text"><p>&nbsp;1.Entrega</p></div>
-                    </li>
-
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 3 ? "active" : "") ?> border-solid-right">
-                        <div class="iconStep step3"></div>
-                        <div class="text"><p>2.Pago</p></div>
-                    </li>
-
-                    <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 4 ? "active" : "") ?>">
-                        <div class="iconStep step4"></div>
-                        <div class="text"><p>3.Confirmación</p></div>
-                    </li>
-                </ul>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="steps">
+                    <ul>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 2 ? "active" : "") ?> border-solid-right">
+                            <div class="iconStep step2"></div>
+                            <div class="text"><p>1.Entrega</p></div>
+                        </li>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 3 ? "active" : "") ?> border-solid-right">
+                            <div class="iconStep step3"></div>
+                            <div class="text"><p>2.Pago</p></div>
+                        </li>
+                        <li class="<?php echo (Yii::app()->params->pagar['pasos'][$paso] == 4 ? "active" : "") ?>">
+                            <div class="iconStep step4"></div>
+                            <div class="text"><p>3.Confirmación</p></div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -33,16 +33,25 @@
 <?php endif; ?>
 
 <section>
-    <div class="container">
+    <div class="container desplegables">
         <div class="row">
-            <div class="col-sm-12 desplegables">
-                <?php $this->renderPartial('_d_paso' . Yii::app()->params->pagar['pasos'][$paso], $parametros); ?>
-                    <?php if ($pasoAnterior !== null): ?>
-                    <button class="editar" id="btn-carropagar-anterior" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoAnterior ?>">Atr&aacute;s</button><
-                    <?php endif; ?>
-                    <?php if ($pasoSiguiente !== null): ?>
-                    <button class="adicionar" id="btn-carropagar-siguiente" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoSiguiente ?>">Continuar</button>
-                    <?php endif; ?>
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php $this->renderPartial('_d_paso' . Yii::app()->params->pagar['pasos'][$paso], $parametros); ?>
+                    </div>
+                </div>
+                <div class="space-1"></div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <?php if ($pasoAnterior !== null): ?>
+                            <button class="editar" id="btn-carropagar-anterior" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoAnterior ?>">Atr&aacute;s</button>
+                        <?php endif; ?>
+                        <?php if ($pasoSiguiente !== null): ?>
+                            <button class="adicionar" id="btn-carropagar-siguiente" data-origin="<?php echo $paso ?>" data-redirect="<?php echo $pasoSiguiente ?>">Continuar</button>
+                        <?php endif; ?>  
+                    </div>
+                </div>
             </div>
         </div>
     </div>
