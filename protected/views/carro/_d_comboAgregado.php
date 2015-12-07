e013a88c5b08baef6e8e5b28622e8bc4dda180f5
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-12-07 16:08:15 -0500

    combo agregado

--- protected/views/carro/_d_comboAgregado.php
+++ protected/views/carro/_d_comboAgregado.php
@@ -4,7 +4,7 @@
               <div class="item"><img class="img-responsive" src="<?php echo Yii::app()->request->baseUrl . $objCombo->rutaImagen(); ?>" ></div>      
         </div>
         <div id="productoAgregado" class="col-md-10">
-            Se ha adicionado <?php echo $objCombo->descripcionCombo ?> al carrito.
+            Se ha adicionado<br> <strong><?php echo $objCombo->descripcionCombo ?></strong> al carrito.
         </div>
     </div> 
 </div>
