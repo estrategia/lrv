0b257d942f2f6d33783ed30c0b14b4dd50bfe630
JeffreyMartinezEiso <jeffrey.martinez@eiso.com.co>
2015-12-10 10:37:46 -0500

    actualizando selects

--- js/desktop.js
+++ js/desktop.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$('#items-page').select2();
+$('body #items-page').select2();
 
 $(document).on('click', 'button[data-role="tipovista-listaproductos"]', function() {
     listaProductoVista($(this).attr('data-type'));
