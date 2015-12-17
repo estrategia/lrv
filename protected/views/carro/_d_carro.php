78799885a7306a387b844906d9e25c1fcb48ef96
msanchez-eiso <miguel.sanchez@eiso.com.co>
2015-12-17 06:53:24 -0500

    Desktop carro compras - botones - Se ajustan estilos y posiciones de botones

--- protected/views/carro/_d_carro.php
+++ protected/views/carro/_d_carro.php
@@ -2,8 +2,22 @@
 <?php $listPositionDelivery = array(); ?>
 <!-- Vista Carro -->
 <section>
-        <div class="row">
-            <div class="col-md-9 border-right">
+    <div class="row">
+        <div class="col-md-9 border-right">
+            <?php if (!$lectura) : ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div>
+                            <div class="space-1"></div>
+                            <?php echo CHtml::link('Vaciar carrito', '#', array('data-role' => 'carrovaciar', 'class' => 'btn btn-default', 'role' => "button")); ?>
+                            <?php echo CHtml::link('Guardar en la lista personal', '#', array('data-role' => 'lstpersonalguardar', 'data-tipo' => 3, 'data-codigo' => 0, 'class' => 'btn btn-default', 'role' => "button")); ?>
+                        </div>
+                    </div>
+                </div>
+            <div class="space-1"></div>
+            <?php endif; ?>
+            <div class="row">
+                <div class="col-md-12">
                     <table class="table table-bordered table-hover table-striped tabla-carro">
                         <thead class="cabecera-tabla">
                             <tr>
@@ -80,37 +94,45 @@
                             </tbody>
                         </table>
                     <?php endif; ?>
-            </div>
-
-            <div class="col-md-3 detalles">
-                <h3>Detalles de la compra</h3>
-                <span>Subtotal <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getCost(), Yii::app()->params->formatoMoneda['moneda']); ?></span><br>
-                <span>Servicio <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getShipping(), Yii::app()->params->formatoMoneda['moneda']); ?></span><br>
-                <?php if (Yii::app()->shoppingCart->getExtraShipping() > 0): ?>
-                    <span>Flete adicional <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getExtraShipping(), Yii::app()->params->formatoMoneda['moneda']); ?></span>
-                <?php endif; ?>
-                <?php if (Yii::app()->shoppingCart->getBono() > 0): ?>
-                    <span>Bono <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getBono(), Yii::app()->params->formatoMoneda['moneda']); ?></span>
-                <?php endif; ?>
-                <h3>Total a pagar <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getTotalCost(), Yii::app()->params->formatoMoneda['moneda']) ?></h3>
-                <?php if (Yii::app()->shoppingCart->getObjCiudad()->excentoImpuestos == 0 && Yii::app()->shoppingCart->getTaxPrice() > 0): ?>
-                    <span>Impuestos incluidos <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getTaxPrice(), Yii::app()->params->formatoMoneda['moneda']) ?></span><br/>
-                <?php endif; ?>
-                <?php if (Yii::app()->shoppingCart->getDiscountPrice(true) > 0): ?>
-                    <span style="color:#EA0001;">Su ahorro  <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getDiscountPrice(true), Yii::app()->params->formatoMoneda['moneda']) ?></span>
-                <?php endif; ?>
-
-                <?php if (!$lectura) : ?>
-                <div class="center">
-                    <br/>
-                    <?php echo CHtml::link('Comprar', $this->createUrl('/carro/pagar'), array('class' => 'btn btn-primary', 'role' => "button")); ?>
-                    <?php echo CHtml::link('Vaciar carrito', '#', array('data-role' => 'carrovaciar', 'class' => 'btn btn-primary', 'role' => "button")); ?>
-                    <br/><br/>
-                    <?php echo CHtml::link('Guardar en la lista personal', '#', array('data-role' => 'lstpersonalguardar', 'data-tipo' => 3, 'data-codigo' => 0, 'class' => 'btn btn-primary', 'role' => "button")); ?>
                 </div>
-                <?php endif; ?>
-
-
             </div>
+            <?php if (!$lectura) : ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div>
+                            <div class="space-1"></div>
+                            <?php echo CHtml::link('Vaciar carrito', '#', array('data-role' => 'carrovaciar', 'class' => 'btn btn-default', 'role' => "button")); ?>
+                            <?php echo CHtml::link('Guardar en la lista personal', '#', array('data-role' => 'lstpersonalguardar', 'data-tipo' => 3, 'data-codigo' => 0, 'class' => 'btn btn-default', 'role' => "button")); ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
         </div>
+
+        <div class="col-md-3 detalles">
+            <h3>Detalles de la compra</h3>
+            <span>Subtotal <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getCost(), Yii::app()->params->formatoMoneda['moneda']); ?></span><br>
+            <span>Servicio <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getShipping(), Yii::app()->params->formatoMoneda['moneda']); ?></span><br>
+            <?php if (Yii::app()->shoppingCart->getExtraShipping() > 0): ?>
+                <span>Flete adicional <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getExtraShipping(), Yii::app()->params->formatoMoneda['moneda']); ?></span>
+            <?php endif; ?>
+            <?php if (Yii::app()->shoppingCart->getBono() > 0): ?>
+                <span>Bono <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getBono(), Yii::app()->params->formatoMoneda['moneda']); ?></span>
+            <?php endif; ?>
+            <h3>Total a pagar <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getTotalCost(), Yii::app()->params->formatoMoneda['moneda']) ?></h3>
+            <?php if (Yii::app()->shoppingCart->getObjCiudad()->excentoImpuestos == 0 && Yii::app()->shoppingCart->getTaxPrice() > 0): ?>
+                <span>Impuestos incluidos <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getTaxPrice(), Yii::app()->params->formatoMoneda['moneda']) ?></span><br/>
+            <?php endif; ?>
+            <?php if (Yii::app()->shoppingCart->getDiscountPrice(true) > 0): ?>
+                <span style="color:#EA0001;">Su ahorro  <?php echo Yii::app()->numberFormatter->format(Yii::app()->params->formatoMoneda['patron'], Yii::app()->shoppingCart->getDiscountPrice(true), Yii::app()->params->formatoMoneda['moneda']) ?></span>
+            <?php endif; ?>
+
+            <?php if (!$lectura) : ?>
+                <div class="center">
+                    <div class="space-1"></div>
+                    <?php echo CHtml::link('Comprar', $this->createUrl('/carro/pagar'), array('class' => 'btn btn-primary', 'role' => "button")); ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
 </section>
