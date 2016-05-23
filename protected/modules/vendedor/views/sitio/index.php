90a3cc1c20060652a2001328e0e4106b0ee4b5b8
msanchez-eiso <>
2016-05-23 11:16:31 -0500

    Bonos tienda exportar

--- protected/modules/vendedor/views/sitio/index.php
+++ protected/modules/vendedor/views/sitio/index.php
@@ -1,5 +1,5 @@
 <center>
-    <a href="<?php echo CController::createUrl('usuario/autenticar') ?>">
+    <a href="<?php echo CController::createUrl('usuario/autenticar') ?>" data-ajax="false">
     <img style="padding-top: 50px;padding-bottom: 50px;" src="/lrv/images/icono-home.png" >
   </a>  
 </center>
