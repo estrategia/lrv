e7ae820723518bb369641b94c981ee0275bc615b
estrategia <andres.tabares@eiso.com.co>
2016-07-14 14:26:53 -0500

    CODIGO DE GOOGLE ANALITYCS A METAMUCIL

--- protected/views/campania/porque-elejir-metamucil.php
+++ protected/views/campania/porque-elejir-metamucil.php
@@ -25,7 +25,20 @@
             .pattern-background{background-image:url(".Yii::app()->request->baseUrl."/images/contenido/metamucil/pattern.png);background-size:cover;}
             .title{background-color: #AB084E;color: #fff; padding: 21px; border-radius: 15px; margin: 0% 25% 0% 25%;text-align: center;}
             .title_m {background-color: #AB084E;color: #fff; padding: 21px; border-radius: 15px; margin: 0;text-align: center;}
-	</style>";
+	</style>
+
+    <!--Codigo google analitycs-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-80765083-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
+    ";
 ?>
 
 <!--Versión movil-->
