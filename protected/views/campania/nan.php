cf388e7b93b0abfce6d63ce4cc45337d3c7c2064
felipePatino <felipe.patino@eiso.com.co>
2016-10-20 16:24:36 -0500

    Código remarketing NAN

--- protected/views/campania/nan.php
+++ protected/views/campania/nan.php
@@ -20,7 +20,21 @@ $this->metaTags = "<meta http-equiv='X-UA-Compatible' content='IE=edge'>
         .compra-online-movil {width: 60%;}
 	</style>
 
-
+  <!-- Google Code para etiquetas de remarketing -->
+  <script type='text/javascript'>
+    /* <![CDATA[ */
+    var google_conversion_id = 872146633;
+    var google_custom_params = window.google_tag_params;
+    var google_remarketing_only = true;
+    /* ]]> */
+  </script>
+  <script type='text/javascript' src='//www.googleadservices.com/pagead/conversion.js'>
+  </script>
+  <noscript>
+  <div style='display:inline;'>
+  <img height='1' width='1' style='border-style:none;' alt='' src='//googleads.g.doubleclick.net/pagead/viewthroughconversion/872146633/?guid=ON&amp;script=0'/>
+  </div>
+  </noscript>
     ";
 ?>
 
