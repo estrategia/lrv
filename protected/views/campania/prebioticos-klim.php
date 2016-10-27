d4b2044eebcbf133d01430e848e1b2a8f540c4a5
felipePatino <felipe.patino@eiso.com.co>
2016-10-27 10:34:18 -0500

    Pagina interna Klim1+

---
+++ protected/views/campania/prebioticos-klim.php
@@ -0,0 +1,121 @@
+<?php $this->pageTitle = "Prebioticos Klim 1+ La Rebaja Virtual"; ?>
+<?php
+$this->metaTags = "<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='description' content=''>
+    <meta name='keywords' content=''>
+  	<style>
+          .background {background-image:url(".Yii::app()->request->baseUrl."/images/contenido/klim1/background.jpg);background-size:cover;}
+          @font-face {
+              font-family:VAGRoundedStd-Bold ;
+              src: url(".Yii::app()->request->baseUrl."/images/contenido/klim1/font/VAGRoundedStd-Bold.otf);
+          }
+          @font-face {
+              font-family:VAGRoundedStd-Light ;
+              src: url(".Yii::app()->request->baseUrl."/images/contenido/klim1/font/VAGRoundedStd-Light.otf);
+          }
+          .texto{font-family:VAGRoundedStd-Light ; font-size:14px;color:#872f01;text-align: center;margin-top: 3%;margin-bottom: 2%;}
+          .text-strong {font-family:VAGRoundedStd-Bold ;}
+          .contentText{max-width: 900px;margin: auto;background-color: #FFF376;padding: 15px;border: 3px solid #905B2F;border-radius: 15px;}
+          .contentText .texto {text-align: justify;font-size: 18px;}
+          .contentText-m{max-width: 85%;margin: auto;background-color: #FFF376;padding: 15px;border: 3px solid #905B2F;border-radius: 15px;}
+          .contentText-m .texto {text-align: justify;font-size: 18px;}
+  	</style>
+      ";
+?>
+<!--Versión movil-->
+<?php if ($this->isMobile): ?>
+    <div class="background">
+        <section>
+          <img src="<?= Yii::app()->request->baseUrl ?>/images/contenido/klim1/producto.png" class="img-responsive" alt="Klim 1+ Fortiprotect" style="margin: 0 auto;display:block;width: 65%;">
+          <a href="">
+              <img style="margin:0 auto;display:block;width: 40%;" src="<?= Yii::app()->request->baseUrl ?>/images/contenido/klim1/btn-comprar.png" class="img-responsive" alt="Comprar Klim1+">
+          </a>
+          <img style="margin: 5% auto;display:block;" src="<?= Yii::app()->request->baseUrl ?>/images/contenido/klim1/entrega-en-1-hora.png" class="img-responsive" alt="Entrega en 1 hora">
+        </section>
+        <section>
+          <div class="contentText-m">
+            <h1 class="text-strong" style="color: #A12F16;text-shadow: 3px 0px 0px #fff;text-align: center;">Prebio<sup>®</sup> 1 el aliado de las defensas de tu hijo.</h1>
+            <p class="texto">
+              Tu hijo ahora es más inquieto y curioso, quiere tocar, mover y meterse todo en la boca, es una etapa de su vida donde es importante ayudarlo a reforzar sus defensas.<p>
+              <p class="texto">
+              Empecemos por saber que convivimos con microorganismos presentes en la boca, la piel y el aparato digestivo, estas bacterias cumplen múltiples funciones destinadas a
+              fortalecer los mecanismos de defensa contra las otras bacterias, llamadas patógenas, que son las que producen enfermedades y alergias. </p>
+                <p class="texto">
+              Por fortuna encontramos sustancias en algunos alimentos que por su constitución no son digeridas en el estómago o en el intestino delgado y llegan al
+              colon para favorecer el crecimiento y mantenimiento de las bacterias de la llamada flora  intestinal normal. A estos alimentos se les conoce con el nombre
+              de prebióticos y la leche materna es el principal ejemplo de ellos. </p>
+                <p class="texto">
+              Uno de los principales alimentos prebióticos es la fibra dietética que se encuentra de modo natural en legumbres, verduras y hortalizas, frutas frescas,
+              frutos secos y cereales de grano entero. </p>
+                <p class="texto">
+              Se ha demostrado que los niños al convivir con las “bacterias saludables” del colon, éstas lo protegerán de infecciones que causan enfermedades del sistema digestivo.</p>
+                <p class="texto">
+              Los prebióticos potencian el sistema inmune natural y por eso se consideran como alimentos funcionales, ya que más que a la nutrición, contribuyen a mejorar la función
+              digestiva y las defensas naturales del organismo. Es así como se ha desarrollado Prebio® 1. Una combinación de prebióticos exclusiva de NESTLÉ® que promueve el
+              crecimiento de microorganismos benéficos en el sistema digestivo de tu pequeño. </p>
+                <p class="texto">
+              Después de conocer qué es Prebio ®1 y cómo beneficia las defensas de tu hijo te recomendamos incluir en su alimentación  KLIM ® 1+ ya que además de tener Prebio® 1,
+              tiene vitaminas y minerales para proteger a tu niño en esta importante edad, en la que necesitan reforzar sus defensas para enfrentar virus y varias enfermedades,
+              desde ahora tú y tu hijo cuentan con el escudo protector de KLIM ® 1+.
+            </p>
+          </div>
+        </section>
+        <section style="margin-top:20px;">
+            <p class="texto"><span class="text-strong">Aviso importante:</span> <br>la leche materna es el mejor alimento para los niños. <br>
+            KLIM® 1+ FORTIPROTECT® es un Alimento Lácteo para niños a partir de 1 año.</p>
+            <div style="height:24px;"></div>
+        </section>
+    </div>
+<!--Version movil-->
+
+<!--Versión escritorio-->
+<?php else: ?>
+    <div class="container background">
+        <div class="row">
+            <div class="col-md-6">
+              <img src="<?= Yii::app()->request->baseUrl ?>/images/contenido/klim1/producto.png" class="img-responsive" alt="Klim 1+ Fortiprotect" style="margin: auto 35%;width: 65%;">
+            </div>
+            <div class="col-md-6">
+              <a href="">
+                  <img style="margin-top: 15%;width: 40%;" src="<?= Yii::app()->request->baseUrl ?>/images/contenido/klim1/btn-comprar.png" class="img-responsive" alt="Comprar Klim1+">
+              </a>
+              <img style="margin-top: 10%;" src="<?= Yii::app()->request->baseUrl ?>/images/contenido/klim1/entrega-en-1-hora.png" class="img-responsive" alt="Entrega en 1 hora">
+            </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="contentText">
+              <h1 class="text-strong" style="color: #A12F16;text-shadow: 3px 0px 0px #fff;text-align: center;">Prebio<sup>®</sup> 1 el aliado de las defensas de tu hijo.</h1>
+              <p class="texto">
+                Tu hijo ahora es más inquieto y curioso, quiere tocar, mover y meterse todo en la boca, es una etapa de su vida donde es importante ayudarlo a reforzar sus defensas.<p>
+                <p class="texto">
+                Empecemos por saber que convivimos con microorganismos presentes en la boca, la piel y el aparato digestivo, estas bacterias cumplen múltiples funciones destinadas a
+                fortalecer los mecanismos de defensa contra las otras bacterias, llamadas patógenas, que son las que producen enfermedades y alergias. </p>
+                  <p class="texto">
+                Por fortuna encontramos sustancias en algunos alimentos que por su constitución no son digeridas en el estómago o en el intestino delgado y llegan al
+                colon para favorecer el crecimiento y mantenimiento de las bacterias de la llamada flora  intestinal normal. A estos alimentos se les conoce con el nombre
+                de prebióticos y la leche materna es el principal ejemplo de ellos. </p>
+                  <p class="texto">
+                Uno de los principales alimentos prebióticos es la fibra dietética que se encuentra de modo natural en legumbres, verduras y hortalizas, frutas frescas,
+                frutos secos y cereales de grano entero. </p>
+                  <p class="texto">
+                Se ha demostrado que los niños al convivir con las “bacterias saludables” del colon, éstas lo protegerán de infecciones que causan enfermedades del sistema digestivo.</p>
+                  <p class="texto">
+                Los prebióticos potencian el sistema inmune natural y por eso se consideran como alimentos funcionales, ya que más que a la nutrición, contribuyen a mejorar la función
+                digestiva y las defensas naturales del organismo. Es así como se ha desarrollado Prebio® 1. Una combinación de prebióticos exclusiva de NESTLÉ® que promueve el
+                crecimiento de microorganismos benéficos en el sistema digestivo de tu pequeño. </p>
+                  <p class="texto">
+                Después de conocer qué es Prebio ®1 y cómo beneficia las defensas de tu hijo te recomendamos incluir en su alimentación  KLIM ® 1+ ya que además de tener Prebio® 1,
+                tiene vitaminas y minerales para proteger a tu niño en esta importante edad, en la que necesitan reforzar sus defensas para enfrentar virus y varias enfermedades,
+                desde ahora tú y tu hijo cuentan con el escudo protector de KLIM ® 1+.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+            <p class="texto"><span class="text-strong">Aviso importante:</span> la leche materna es el mejor alimento para los niños. <br>
+            KLIM® 1+ FORTIPROTECT® es un Alimento Lácteo para niños a partir de 1 año.</p>
+        </div>
+    </div>
+    <!--Fin versión escritorio-->
+<?php endif; ?>
