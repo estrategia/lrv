31a29c13303a050e684c5a4d2b932b70658e1dbe
msanchez-eiso <miguel.sanchez@eiso.com.co>
2016-10-31 15:09:43 -0500

    Merge

--- protected/config/main.php
+++ protected/config/main.php
@@ -73,7 +73,7 @@ return array(
         				'components' => array(
         						'user' => array(
         								'class' => 'puntoventa.components.UserPuntoVenta',
-        						),
+                                 ),
         				)),
         			
         		
