74917644aa2115cdc05b3a1a7e1586da16a45e42
msanchez-eiso <miguel.sanchez@eiso.com.co>
2016-11-03 08:33:24 -0500

    Campañas - URL

---
+++ protected/controllers/PublicidadController.php
@@ -0,0 +1,34 @@
+<?php
+
+class PublicidadController extends Controller {
+
+    /**
+     * @return array action filters
+     * */
+    public function filters() {
+        return array(
+            array(
+                'application.filters.SessionControlFilter + contenido',
+                'isMobile' => $this->isMobile,
+                'redirect' => false
+            ),
+        );
+    }
+    
+    public function actionContenido($nombre) {
+    	if (empty($nombre))
+    		throw new CHttpException(404, 'Solicitud inválida. Nombre requerido');
+    
+    		if(!is_readable(Yii::getPathOfAlias("application.views.campania.$nombre").".php")){
+    			throw new CHttpException(404, 'Campaña no existente.');
+    		}
+    
+    		try{
+    			$this->render("application.views.campania.$nombre");
+    		}  catch (Exception $exc){
+    			Yii::log($exc->getMessage(),  CLogger::LEVEL_WARNING);
+    			throw new CHttpException(404, "Error al abrir campaña");
+    		}
+    }
+
+}
