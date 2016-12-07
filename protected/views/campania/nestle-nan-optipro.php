91cc232a05a9e02d142d5cf86168acb08f158b84
felipePatino <felipe.patino@eiso.com.co>
2016-12-07 07:46:11 -0500

    Pixel facebook micrositio NAN

--- protected/views/campania/nestle-nan-optipro.php
+++ protected/views/campania/nestle-nan-optipro.php
@@ -41,6 +41,34 @@ $this->metaTags = "<meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <img height='1' width='1' style='border-style:none;' alt='' src='//googleads.g.doubleclick.net/pagead/viewthroughconversion/872146633/?guid=ON&amp;script=0'/>
   </div>
   </noscript>
+
+  <!-- Facebook Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window,document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+   fbq('init', '1237970366269442');
+  fbq('track', 'PageView');
+  </script>
+  <noscript>
+   <img height='1' width='1'
+  src='https://www.facebook.com/tr?id=1237970366269442&ev=PageView
+  &noscript=1'/>
+  </noscript>
+  <!-- End Facebook Pixel Code -->
+
+  <script>
+  fbq('track', 'ViewContent', {
+  value: 3.50,
+  currency: 'USD'
+  });
+  </script>
+  
     ";
 ?>
 
