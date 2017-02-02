61619965e63438b0a74cdbafde81bef602ca9431
msanchez-eiso <miguel.sanchez@eiso.com.co>
2017-02-02 14:08:26 -0500

    Estilos select ciudad
    En mapa desktop y entrega nacional ubicacion

--- js/desktop.js
+++ js/desktop.js
@@ -1996,7 +1996,7 @@ $(document).on('click', 'button[data-role="ubicacion-mapa"]', function() {
                 $.getScript("https://maps.googleapis.com/maps/api/js?client=" + gmapKey).done(function(script, textStatus) {
                     $.getScript(requestUrl + "/js/ubicacion.min.js").done(function(script, textStatus) {
                         $('#main-page').append(data);
-                        // $('#select-ubicacion-psubsector .ciudades').select2();
+                        $('#select-ubicacion-psubsector .ciudades').select2();
                         inicializarMapa();
                         $('#modal-ubicacion-map').modal('show');
                         // $('#select-ubicacion-content').show();
