3f814f1d97e37b233622a427e2e2054249baa0e3
msanchez-eiso <miguel.sanchez@eiso.com.co>
2017-10-31 10:19:50 -0500

    database conflict

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,11 +1,7 @@
 <?php
 
 return array(
-<<<<<<< HEAD
-    // 'connectionString' => 'mysql:host=192.168.1.29;port=3306;dbname=lrvmovil_copservir',
-=======
     'connectionString' => 'mysql:host=192.168.1.30;port=3306;dbname=lrvmovil_copservir',
->>>>>>> 8ede6ccac3075b6e0c613cc70fc41479ed3d9210
     //'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrvmovil_copservir',
     'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrv',
     'username' => 'root',
