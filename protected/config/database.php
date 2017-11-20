3f2b53cd7ebd54e17d22824f811ff2d6bc72806a
felipePatino <felipe.patino@eiso.com.co>
2017-11-20 14:23:44 -0500

    klim1+ deslactosado

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-	//'connectionString' => 'mysql:host=192.168.1.10;port=3306;dbname=lrvmovil_copservir',
-		'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrv1017',
+		//'connectionString' => 'mysql:host=192.168.1.38;port=3306;dbname=lrvmovil_copservir',
+		'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrv',
 
     'username' => 'root',
     // 'password' => '',
