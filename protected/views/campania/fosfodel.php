f84b1156ba573310f7070b9b7e8b8e85500051d7
felipePatino <felipe.patino@eiso.com.co>
2017-12-22 08:07:32 -0500

    Remarketing Padel

--- protected/views/campania/fosfodel.php
+++ protected/views/campania/fosfodel.php
@@ -68,6 +68,22 @@
 ";
 ?>
 
+<!-- Google Code para etiquetas de remarketing -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 966686026;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/966686026/?guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 <!-- VERSION MOVIL -->
 <?php if ($this->isMobile): ?>
   <div class="main-bg-m" style="padding: 25px;">
