93d490d39411732525ea3e54aac98d67f0012099
felipePatino <felipe.patino@eiso.com.co>
2018-01-24 10:26:56 -0500

    Actualización fosfodel

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,9 +2,9 @@
 
 return array(
 
-	// 'connectionString' => 'mysql:host=192.168.1.30;port=3306;dbname=lrvmovil_copservir',
-    'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrvmovil_copservir',
-	'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrv1017',
+	'connectionString' => 'mysql:host=192.168.1.30;port=3306;dbname=lrvmovil_copservir',
+  //  'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrvmovil_copservir',
+	//'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrv1017',
     'username' => 'root',
     // 'password' => '',
     'charset' => 'UTF8',
