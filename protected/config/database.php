fcd081b68dd3f09dec5a6c10a63d9464e1e91a17
msanchez-eiso <miguel.sanchez@eiso.com.co>
2018-01-31 23:50:30 -0500

    Cliente fiel - Mensajes correo

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,14 +1,8 @@
 <?php
 
 return array(
-<<<<<<< HEAD
-	//'connectionString' => 'mysql:host=192.168.1.11;port=3306;dbname=lrvmovil_copservir',
-	'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrvmovil_copservir',
-
-=======
 	'connectionString' => 'mysql:host=192.168.1.15;port=3306;dbname=lrvmovil_copservir',
-	// 'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrvmovil_copservir',
->>>>>>> f54c1c1fa8bc25874606876782b45c88a793486b
+	//'connectionString' => 'mysql:host=localhost;port=3306;dbname=lrvmovil_copservir',
     'username' => 'root',
     // 'password' => '',
     'charset' => 'UTF8',
