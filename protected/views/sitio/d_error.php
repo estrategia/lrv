a9a42045bdc5a569e2389630e00b1c548b3fd6bd
felipePatino <felipe.patino@eiso.com.co>
2018-02-01 17:05:05 -0500

    Error 404

---
+++ protected/views/sitio/d_error.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <center class="info-404">
+      <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        <h1>404. PÁGINA NO ENCONTRADA</h1>
+        <hr>
+        <p>Se present&oacute; una falla al cargar la p&aacute;gina que solicitaste, <br> es posible que la entrada haya sido eliminada o la direcci&oacute;n no exista.</p>
+        <a class="btn btn-primary btn-lg" href="<?php echo $this->createUrl('/sitio/inicio') ?>">Ir al inicio</a>
+    </center>
+</div>
